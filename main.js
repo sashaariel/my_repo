3a0066686721833918ac844497edf5797fd0eba1
sashaariel <sashaariel@live.cl>
2022-10-06 11:49:26 -0300

    Adicion de promt y calculo de numero de orden de compra con JS

---
+++ main.js
@@ -0,0 +1,19 @@
+// Prompt
+
+let nombreingresado = prompt ("ingrese su nombre")
+var blanco = " "
+if (nombreingresado != ""){
+alert ("Hola" + blanco + nombreingresado + " Bienvenidx a Hera" );}
+else {
+    alert ( "Ingresa un Nombre")
+}
+
+//Operacion y calculo de N° de Orden de Compra
+
+for (let i = 1; i <= 10; i++) {
+    // En cada repetición solicitamos un nombre.
+    let ingresarNombre = prompt("Ingresar nombre");
+    // Informamos el turno asignado usando el número de repetición (i).
+    alert(" Tu número de orden de compra es "+i+" Nombre: "+ingresarNombre);
+   }
+
