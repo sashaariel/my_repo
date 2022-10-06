26e8069dfaea50571acdef42e3cb9688ce743d6f
sashaariel <sashaariel@live.cl>
2022-10-06 14:02:54 -0300

    modifique el js en la cantidad de numeros de orden de compra, también hice correcciones de las imagenes para que se vean correctamente, además borré los br de ropa interior

--- main.js
+++ main.js
@@ -3,17 +3,17 @@
 let nombreingresado = prompt ("ingrese su nombre")
 var blanco = " "
 if (nombreingresado != ""){
-alert ("Hola" + blanco + nombreingresado + " Bienvenidx a Hera" );}
+alert ("Hola" + blanco + nombreingresado + ", bienvenidx a Hera" );}
 else {
     alert ( "Ingresa un Nombre")
 }
 
 //Operacion y calculo de N° de Orden de Compra
 
-for (let i = 1; i <= 10; i++) {
+for (let i = 1; i <= 3; i++) {
     // En cada repetición solicitamos un nombre.
     let ingresarNombre = prompt("Ingresar nombre");
     // Informamos el turno asignado usando el número de repetición (i).
-    alert(" Tu número de orden de compra es "+i+" Nombre: "+ingresarNombre);
+    alert(" Tu número de orden de compra es "+i+", Nombre: "+ingresarNombre);
    }
 
