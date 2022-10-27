ad091710bee8cf56bc08097898d513f88c563775
sashaariel <sashaariel@live.cl>
2022-10-27 15:46:16 -0300

    corrección en nombre de index inicio en el link de ingreso

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 // Prompt
 
-let nombreingresado = prompt ("ingrese su nombre")
+let nombreingresado = prompt("ingrese su nombre")
 var blanco = " "
-if (nombreingresado != ""){
-alert ("Hola" + blanco + nombreingresado + ", bienvenidx a Hera" );}
+if (nombreingresado != "") {
+    alert("Hola" + blanco + nombreingresado + ", bienvenidx a Hera");
+}
 else {
-    alert ( "Ingresa un Nombre")
+    alert("Ingresa un Nombre")
 }
 
 //Operacion y calculo de N° de Orden de Compra
@@ -14,8 +15,21 @@ for (let i = 1; i <= 3; i++) {
     // En cada repetición solicitamos un nombre.
     let ingresarNombre = prompt("Ingresar nombre");
     // Informamos el turno asignado usando el número de repetición (i).
-    alert(" Tu número de orden de compra es "+i+", Nombre: "+ingresarNombre);
-   }
+    alert(" Tu número de orden de compra es " + i + ", Nombre: " + ingresarNombre);
+}
 
 
-   console.log(13 % 5)
+//   Calcular precio
+
+// busqueda de productos
+
+const ropaInterior = 
+[{ id: 1, producto: "Bombacha", precio: 125 },
+{ id: 2, producto: "Corpiño", precio: 70 },
+{ id: 3, producto: "", precio: 50 },
+{ id: 4, producto: "Flan", precio: 100 }];
+
+const buscarPan = ropaInterior.find(producto => producto.id === 3);
+console.log(buscarPan)
+
+
