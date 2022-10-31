d43089433f5a21037fb7dd5f8f9b034efa871bd9
sashaariel <sashaariel@live.cl>
2022-10-31 11:11:27 -0300

    modificaciones de posición de buscador en el menú

--- main.js
+++ main.js
@@ -63,17 +63,6 @@ console.log(buscaropaInterior)
 
 // Array 
 
-var d=new Date();
-var dia=new Array(7);
-dia[0]="Domingo";
-dia[1]="Lunes";
-dia[2]="Martes";
-dia[3]="Miercoles";
-dia[4]="Jueves";
-dia[5]="Viernes";
-dia[6]="Sabado";
-document.write("Hoy es: " + dia[d.getDay()]);
-
 //BUSCADOR DE CONTENIDO
 
 document.getElementById("icon-search").addEventListener("click", mostrar_buscador);
